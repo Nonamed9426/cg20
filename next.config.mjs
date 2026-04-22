8452de49a6f2259631232f7cb03eedbb132265ff
Heon <jindalle1585@naver.com>
2026-04-22 16:19:18 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,26 @@ const nextConfig = {
         hostname: 'cdn.akamai.steamstatic.com'
       }
     ]
+  },
+  experimental: {
+    allowedDevOrigins: [
+      "192.168.56.120:3000",      // 가상머신 IP
+      "192.168.0.9:3000",         // 가상머신 IP
+      "localhost:3000",           // 로컬호스트
+      "127.0.0.1:3000",           // 로컬호스트 IP
+      "crazygames20.heon.me",     // 실제 도메인 (포트가 80/443인 경우 도메인만)
+      "api-crazygames20.geki.moe" // 서브 도메인
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/_next/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+        ],
+      },
+    ];
   }
 };
 
